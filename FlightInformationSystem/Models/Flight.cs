a38fa0ea3dd7927f88682fcbf3aec243350003ef
Dmytro <zaiats.dmytro@student.uzhnu.edu.ua>
2025-06-11 16:37:13 +0300

    AddedModelsFolderAndFlightModel

---
+++ FlightInformationSystem/Models/Flight.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightStorageService.Models
+{
+    public class Flight
+    {
+        public string FlightNumber { get; set; } = string.Empty;
+        public DateTime DepartureDateTime { get; set; }
+        public string DepartureAirportCity { get; set; } = string.Empty;
+        public string ArrivalAirportCity { get; set; } = string.Empty;
+        public int DurationMinutes { get; set; }
+    }
+}
